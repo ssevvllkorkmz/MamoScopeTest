96debb9a2ab3b03bff3df42800217c7d05e4f901
sevvlkorkmz <svb.kormaz1485@gmail.com>
2026-07-01 13:47:52 +0300

    son3

--- MamoScopeTest/AppDbContext.cs
+++ MamoScopeTest/AppDbContext.cs
@@ -1,24 +1,26 @@
 ﻿using MamoScopeTest.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MamoScopeTest
 {
     public class AppDbContext : DbContext
     {
+        
         public DbSet<MotorDriver> MotorDriver { get; set; }
-        public DbSet<MotorDriver> Id { get; set; }
-        public DbSet<MotorDriver> SerialNumber { get; set; }
-        public DbSet<MotorDriver> TestDate { get; set; }
-        public DbSet<MotorDriver> Voltage { get; set; }
-        public DbSet<MotorDriver> IsPassed { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MamoScopeDb;Trusted_Connection=True;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=MamoScopeDb;Trusted_Connection=True;TrustServerCertificate=True;",
+                sqlServerOptionsAction: sqlOptions =>
+                {
+                    
+                    sqlOptions.EnableRetryOnFailure(
+                        maxRetryCount: 5,
+                        maxRetryDelay: TimeSpan.FromSeconds(30),
+                        errorNumbersToAdd: null);
+                });
         }
     }
 }
